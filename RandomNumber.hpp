01f35e5e171babc0d0c6e64225e148f645c0dcf5
Lucas Turci <lucas.turci@gmail.com>
2019-10-21 17:36:52 -0300

    Make Singleton class RandomNumber, that has functions randInt and randDouble, so that we dont need to create a mt19937 rng everytime

---
+++ RandomNumber.hpp
@@ -0,0 +1,48 @@
+#ifndef RANDOM_NUMBER_HPP
+#define RANDOM_NUMBER_HPP
+
+#include <cstdlib>
+#include <chrono>
+#include <ctime>
+#include <random>
+#include <climits>
+
+using std::mt19937;
+
+class RandomNumber {
+public:
+    mt19937 rng;
+    RandomNumber() {
+        rng = mt19937(std::chrono::steady_clock::now().time_since_epoch().count());
+    }
+
+    mt19937 get_rng() {
+        return rng;
+    }
+
+    double randDouble(double l, double r) {
+        std::uniform_real_distribution<double> ur(l, r);
+        return ur(rng); 
+    }
+
+    float randDouble(float l, float r) {
+        std::uniform_real_distribution<float> ur(l, r);
+        return ur(rng);
+    }
+
+    int randInt(int l = INT_MIN, int r = INT_MAX) {
+        std::uniform_int_distribution<int> ui(l, r);
+        return ui(rng);
+    }
+
+    static RandomNumber * getGenerator() {
+        static RandomNumber * gen = NULL;
+
+        if(!gen) {
+            gen = new RandomNumber();
+            return gen;
+        }
+    }
+};
+
+#endif
